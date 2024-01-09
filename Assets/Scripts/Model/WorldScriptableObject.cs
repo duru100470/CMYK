65628b368829f7118a4ddd4b15beaf262a627167
duru100470 <zenpia@naver.com>
2024-01-10 02:45:42 +0900

    WorldSO 세부 구현

--- Assets/Scripts/Model/WorldScriptableObject.cs
+++ Assets/Scripts/Model/WorldScriptableObject.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using BasicInjector;
 using Cysharp.Threading.Tasks;
 using TMPro;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "new World", menuName = "Models/World Data")]
@@ -12,18 +14,62 @@ public class WorldScriptableObject : ScriptableObject
     [HideInInspector]
     [Inject]
     public GameSetting _setting;
+    private bool _isLoaded = false;
+    public bool IsLoaded => _isLoaded;
 
     public int Index;
     public List<MapInfo> Maps;
-    public List<int> Requires;
+    public List<int> Requirements;
 
     public async UniTaskVoid LoadAsync()
     {
+        await LoadClearDataAsync();
+        await LoadMapDataAsync();
+
+        _isLoaded = true;
+    }
+
+    public async UniTask LoadClearDataAsync()
+    {
         await UniTask.WaitUntil(() => _setting.IsAvailable == true);
+
+        var clearString = "";
+
+        if (_setting.WorldClearData.Count - 1 < Index)
+        {
+            for (int i = 0; i < Maps.Count; i++)
+                clearString += "0";
+
+            _setting.WorldClearData.Add(clearString);
+            _setting.Save();
+        }
+        else
+        {
+            clearString = _setting.WorldClearData[Index];
+        }
 
         foreach (var map in Maps)
         {
-            // TODO: Load ClearData from GameSetting
+            map.IsClear = clearString[map.Index - 1] == 1;
+
+            var isAvailable = false;
+
+            foreach (var r in map.Requirements)
+                isAvailable &= clearString[r - 1] == 1;
+
+            map.IsAvailable = isAvailable;
+        }
+    }
+
+    public async UniTask LoadMapDataAsync()
+    {
+        foreach (var map in Maps)
+        {
+            var asset = await Resources.LoadAsync<TextAsset>($"Worlds/{Index}/Map{map.Index}.json") as TextAsset;
+
+            var data = new MapData();
+            data.ImportData(asset.text);
+            map.Data = data;
         }
     }
 }
@@ -33,6 +79,9 @@ public class MapInfo
 {
     public int Index;
     public MapData Data;
-    public List<int> Requires;
+    public List<int> Requirements;
+    [ReadOnly]
     public bool IsClear;
+    [ReadOnly]
+    public bool IsAvailable;
 }
