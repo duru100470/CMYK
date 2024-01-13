e3ff94cb7adcab0aa9313dcc2613d1902d194763
duru100470 <zenpia@naver.com>
2024-01-13 16:11:41 +0900

    기본 기믹 프리팹 구현

--- Assets/Scripts/Puzzle/Object/Rock.cs
+++ Assets/Scripts/Puzzle/Object/Rock.cs
@@ -6,26 +6,29 @@ using UnityEngine;
 public class Rock : MapObject, IMoveable
 {
     private Transform _transform;
+
     private void Awake()
     {
         _transform = GetComponent<Transform>();
     }
+
     public void Move(Coordinate dir)
     {
         Coordinate += dir;
         _transform.position = Coordinate.CoordinateToWorldPoint(Coordinate);
     }
+
     public bool TryMove(Coordinate dir)
     {
         var target = Coordinate + dir;
 
-        if(MapModel.TryGetObject(target, out var obj))
+        if (MapModel.TryGetObject(target, out var obj))
         {
-            if(obj.Info.Type == ObjectType.Wall)
+            if (obj.Info.Type == ObjectType.Wall)
                 return false;
-            if(obj is IMoveable)
+            if (obj is IMoveable)
                 return false;
-            if(obj is IObtainable)
+            if (obj is IObtainable)
                 MapModel.RemoveMapObject(obj);
         }
 
