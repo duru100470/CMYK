ad671a8094eeb526121da2fa25f881d352fa9d53
ysm436 <endtlr2004@gmail.com>
2024-02-04 04:31:12 +0900

    Key 수정
    이벤트 채널 삭제

--- Assets/Scripts/Puzzle/Object/Key.cs
+++ Assets/Scripts/Puzzle/Object/Key.cs
@@ -1,14 +1,5 @@
-using BasicInjector;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using MessageChannel;
-using Cysharp.Threading.Tasks;
-
 public class Key : MapObject, IObtainable
 {
-    [Inject]
-    public MessageChannel.Channel<ColorChangeEvent> colorChannel;
     public void Obtain()
     {
         foreach(var keyDoor in MapModel.GetObjectsByInfo(new ObjectInfo {Type = ObjectType.KeyDoor , Color = Info.Color}))
