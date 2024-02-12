9391637f48a012d386c53460988f582efebd0d76
Hena <hena0914@korea.ac.kr>
2024-02-12 23:50:30 +0900

    월드 선택 1차 구현

--- Assets/Scripts/Main/MainPlayer.cs
+++ Assets/Scripts/Main/MainPlayer.cs
@@ -8,10 +8,13 @@ using UnityEngine.SceneManagement;
 public class MainPlayer : MapObject, IInitializable
 {
     private Transform _transform;
+    private int sceneIndex;
 
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(sceneIndex);
     }
 
     public override void Initialize()
@@ -36,12 +39,58 @@ public class MainPlayer : MapObject, IInitializable
     {
         Coordinate += dir;
         _transform.position = Coordinate.CoordinateToWorldPoint(Coordinate);
-    }
 
-    void OnBecameInvisible()    //각 world마다 WorldScene을 만들고 그 스크립트에 넣어야 할 부분 + 추가로 전 씬으로 되돌아오는 기능 구현, 위아래로 못 나가게 구현, world 선택 좌표와 상호작용 구현
-    {
-        SceneManager.LoadScene("World1");
-        Coordinate = new Coordinate(-11, -2);
-        _transform.position = Coordinate.CoordinateToWorldPoint(Coordinate);
+        Vector3 pos = Camera.main.WorldToViewportPoint(_transform.position);
+
+        if (pos.x < 0f)
+        {
+            sceneIndex = sceneIndex - 1;
+            if (sceneIndex < 1)
+            {
+                sceneIndex = 1;
+                pos.x = 0f;
+                _transform.position = Camera.main.ViewportToWorldPoint(pos);
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+                Coordinate = new Coordinate(11, -2);
+                _transform.position = Coordinate.CoordinateToWorldPoint(Coordinate);
+                if (sceneIndex == 1)
+                {
+                    Destroy(gameObject);
+                }
+            }
+            
+        }
+        if (pos.x > 1f)
+        {
+            sceneIndex = sceneIndex + 1;
+            if (sceneIndex > 5)
+            {
+                sceneIndex = 5;
+                pos.x = 1f;
+                _transform.position = Camera.main.ViewportToWorldPoint(pos);
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+                Coordinate = new Coordinate(-11, -2);
+                _transform.position = Coordinate.CoordinateToWorldPoint(Coordinate);
+            }
+        }
+        if (pos.y < 0f)
+        {
+            pos.y = 0f;
+            _transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
+        if (pos.y > 1f)
+        {
+            pos.y = 1f;
+            _transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
     }
+//world 선택 좌표와 상호작용 구현(MapPortal 사용)
+//화면 가장자리까지 간 뒤 해당 방향으로 몇 번 이동 입력하면 반대 방향 움직임이 그 횟수만큼 씹힘
+//MainScene으로 돌아갈 때 ProjectScope 2개임
 }
