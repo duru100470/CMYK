545ca9b5c57ea7ea33a2f6c97da1fabb78ed3c89
DIA24 <5084kkm@naver.com>
2024-02-15 22:35:23 +0900

    월드 챕터간 이동 씬전환 구현

--- Assets/Scripts/Chapter/MapPortal.cs
+++ Assets/Scripts/Chapter/MapPortal.cs
@@ -6,6 +6,7 @@ using MessageChannel;
 using UnityEngine.UI;
 using static Unity.Collections.AllocatorManager;
 using UnityEngine.SceneManagement;
+using Unity.Collections.LowLevel.Unsafe;
 
 public class MapPortal : MapObject, IObtainable
 {
@@ -41,6 +42,7 @@ public class MapPortal : MapObject, IObtainable
 
     public void Obtain()
     {
+        Debug.Log("checkin");
         if (!_lock)
         {
             _dataEffect.AnimationOn();
@@ -66,22 +68,38 @@ public class MapPortal : MapObject, IObtainable
         }
         else if (Input.GetKeyDown(KeyCode.Space) && _isMainScene && _inPlayer)
         {
-            SceneManager.LoadScene($"World{World}");
+            switch (World)
+            {
+                case 0:
+                    SceneLoader.Instance.LoadSceneAsync<ChapterScene0>(null).Forget();
+                    break;
+                case 1:
+                    SceneLoader.Instance.LoadSceneAsync<ChapterScene1>(null).Forget();
+                    break;
+                case 2:
+                    SceneLoader.Instance.LoadSceneAsync<ChapterScene2>(null).Forget();
+                    break;
+                default:
+                    break;
+            }
+            
         }
 
-        if (!_init)
+        if (!_init && _worldLoader != null)
         {
             _mapStatus = _worldLoader.GetStatus(World, Chapter);
 
             switch (_mapStatus)
             {
                 case MapStatus.Cleared:
+                    Debug.Log("clear");
                     _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 1);
                     _spriteRenderer.sprite = _sprites[0];
                     _lock = false;
                     _init = true;
                     break;
                 case MapStatus.Available:
+                    Debug.Log("available");
                     _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 1);
                     _spriteRenderer.sprite = _sprites[1];
                     _lock = false;
