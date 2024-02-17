7ff57d7534c13b17c5f6f280a4f94400c8fa0f89
duru100470 <zenpia@naver.com>
2024-02-17 19:57:41 +0900

    소리 설정 패널 구현

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -9,42 +9,20 @@ public class StartScene : SceneScope, IScene
     public SceneScope SceneScope => this;
 
     [SerializeField]
-    private MapController _mapController;
-    [SerializeField]
-    private TestView _testView;
-
-    [HideInInspector]
-    [Inject]
-    public GameSetting _gameSetting;
-    [Inject]
-    public WorldLoader _worldLoader;
+    private GameObject _inject;
 
     public override void Load(object param)
     {
         base.Load();
-        Debug.Log("Main scene is loaded!");
-
-        LoadAsync().Forget();
+        GameObjectInjector.InjectSingle(_inject, Container);
         SceneLoader.Instance.LoadSceneAsync<MainScene>(null).Forget();
     }
 
     public override void Unload()
     {
-        Debug.Log("Main scene is unloaded!");
-    }
-
-    private async UniTaskVoid LoadAsync()
-    {
-        await (_gameSetting.LoadAsync(), _worldLoader.InitWorlds(_gameSetting));
     }
 
     public override void InitializeContainer(ContainerBuilder builder)
     {
-        builder.AddSingleton<MapData>(null);
-        builder.AddSingletonAs<MapModel, IMapModel>();
-        builder.AddSingleton<MapController>(_mapController);
-        builder.AddSingleton<TestView>(_testView);
-        builder.AddSingleton<MessageChannel.Channel<PlayerEvent>>();
-        builder.AddSingleton<MessageChannel.Channel<PlayerMoveEvent>>();
     }
 }
