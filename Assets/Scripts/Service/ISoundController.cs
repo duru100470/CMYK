7add2a768ce8bd9175f6a0908a9f9cf8bbec2cc1
duru100470 <zenpia@naver.com>
2024-02-17 18:29:50 +0900

    작업 stash

--- Assets/Scripts/Service/ISoundController.cs
+++ Assets/Scripts/Service/ISoundController.cs
@@ -3,6 +3,7 @@ public interface ISoundController
     void PlayEffect(SFXType type, float volume, float pitch);
     void PlayBGM(BGMType type);
     void StopBGM();
+    void ChangeBGMVolume();
 }
 
 public enum SFXType
